c7628306846f8c2cc53423f5d7852ecef0d9cc08
Samuel Mesquita da Fonseca <75632669+samuelMesquita@users.noreply.github.com>
2022-10-20 16:01:09 -0300

    feat: implementação da tela peopleDetails

---
+++ src/screen/PeoplePages.js
@@ -0,0 +1,45 @@
+import { StyleSheet, View } from 'react-native';
+import React from 'react';
+
+import axios from 'axios';
+
+import Pessoas from '../../component/Pessoas';
+
+export default class PeoplePages extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = { people: [] }
+  }
+
+  componentDidMount(){
+    axios
+    .get('https://randomuser.me/api/?nat=br&results=8')
+    .then(response =>{
+        const { results } = response.data;
+        this.setState({
+          people: results
+        });
+    })
+  }
+
+
+  render(){
+    return (
+      <View style={styles.container}>
+        <Pessoas peopleList={this.state.people} ></Pessoas>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container:{
+    backgroundColor: '#202124',
+    height: 1000
+  },
+  PessoaContainer:{
+    flex: 0.3,
+    justifyContent: 'center'
+  }
+});
